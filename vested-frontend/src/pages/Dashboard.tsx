dae2de0d11de6b6ad88a80733540d32f16b60b4f
Ricky Zhang <rickyzhang196@outlook.com>
2024-09-28 11:53:42 -0400

    add table page

--- vested-frontend/src/pages/Dashboard.tsx
+++ vested-frontend/src/pages/Dashboard.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import styles from '../styles/Dashboard.module.css';
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
+import Table from '../components/Table';
 
 const Dashboard: React.FC = () => {
   return (
     <div className={styles.dashboardContainer}>
       <Header />
       <Sidebar />
+      <Table />
     </div>
   );
 };
