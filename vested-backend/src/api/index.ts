8c9fc0d9f5570c8a143be5c2b18145876091157d
Cole Smith <colesmith5400@gmail.com>
2024-09-28 10:12:30 -0400

    deploy

--- vested-backend/src/api/index.ts
+++ vested-backend/src/api/index.ts
@@ -5,11 +5,14 @@ import { getCsvRowByTicker } from "../controllers/csvController";
 import { getStockPrices } from "../controllers/yahooController";
 
 const app = express();
+const PORT = 3000;
 
 app.use(express.json());
 app.use(cors());
 
 app.get("/api/csv/:ticker", getCsvRowByTicker);
 app.get("/api/yahoo/:ticker", getStockPrices);
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 export default app;
