0716b7a177346dd42dcec8d1dff234bf17623c49
Cole Smith <colesmith5400@gmail.com>
2024-09-28 09:20:08 -0400

    cors

--- vested-backend/src/server.ts
+++ vested-backend/src/server.ts
@@ -1,20 +1,22 @@
 import express, { Request, Response } from "express";
-import homeRoutes from './routes/homeRoutes';
-import csvRoutes from './routes/csvRoutes';
-import yahooRoutes from './routes/yahooRoutes';
+import cors from "cors";
+import homeRoutes from "./routes/homeRoutes";
+import csvRoutes from "./routes/csvRoutes";
+import yahooRoutes from "./routes/yahooRoutes";
 
 const app = express();
 const port: number = 3000;
 
-app.use(express.json()); // Middleware to parse JSON
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello Vendure");
 });
 
-app.use('/api/home', homeRoutes);
-app.use('/api/csv', csvRoutes);
-app.use('/api/yahoo', yahooRoutes);
+app.use("/api/home", homeRoutes);
+app.use("/api/csv", csvRoutes);
+app.use("/api/yahoo", yahooRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
